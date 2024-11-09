bacc50c0f792dd6badd80eafcf76f2c641075b44
ashleymmedina <ashley.meza-medina@csedge.org>
2024-11-09 14:32:23 -0800

    part 2

--- translator.py
+++ translator.py
@@ -1,1 +1,58 @@
-print("hello")
+from emoji_translate.emoji_translate import Translator
+import random
+
+emo = Translator(exact_match_only=False)
+
+emoji = {
+    "happy" : "😄",
+    "sad" : "😔" ,
+    "funny" : "😂",
+    "side eye" : "👀",
+    "funny" : "😭",
+    "oh-" : "😕",
+    "idk" : "🤷🏽‍♀️", 
+    "okay" : "🫡",
+    "like" : "🥰", 
+    # "like" : "🤭",
+    # "like" : "😘",
+    "love" : "😍",
+    "love" : "❤️",
+    "don't" : "❌",
+    "don't" : "🙅‍♂️",
+    "don't" : "🙅‍♀️",
+    "dogs" : "🐶",
+    "dog" : "🐾",
+    "cat" : "🐱",
+    "cats" : "😸",
+    "excited" : "🤗",
+    "thank you" : "😊",
+    "appreciate" : "😊",
+    "beautiful" : "🤩",
+    "beautiful" : "😍",
+    "butterfly" : "🦋",
+    "butterflies" : "🦋",
+    "ghetto" : "", 
+    "cool" : "😎", 
+    "cool" : "👍", 
+    "wow" : "", 
+    "wow" : "", 
+    "" : "", 
+    "" : "", 
+    "" : "", 
+    "" : "", 
+    "" : "", 
+}
+
+
+while True:
+    inp = input("Enter a sentence: ")
+    words = inp.split(' ')
+    emojified = ""
+    for word in words:
+        if word in emoji:
+            emojified += emoji[word]
+        else:
+            emojified += word
+        emojified += " " 
+
+    print(emojified)
